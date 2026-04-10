b650b6cd6fd64a4b2aa39b3dcceb1973b5422daf
Brad_Wu <Brad_Wu@wistron.com>
2026-04-10 13:41:13 +0800

    工具列不會再出現程式列 在右下角建立系統程式，並加上關閉的按鈕

--- BoshiInput.py
+++ BoshiInput.py
@@ -6,7 +6,7 @@ from MainFrame import MainFrame
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-
+    app.setQuitOnLastWindowClosed(False)
     window = MainFrame()
     window.show()
     sys.exit(app.exec())
--- MainFrame.py
+++ MainFrame.py
@@ -1,5 +1,6 @@
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QMainWindow, QSystemTrayIcon, QMenu, QApplication
 from PySide6.QtCore import Qt, QPoint, QRect, QSize
+from PySide6.QtGui import QAction
 
 from setting import LoadPNG, png_Boshi
 from Config import Config
@@ -9,12 +10,13 @@ from CommonTool import fromQPoint, toQPoint
 class MainFrame(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint | Qt.WindowType.Tool)
         self.setWindowIcon(LoadPNG(png_Boshi))
 
         self._config = Config()
 
         self._restorePosition()
+        self._create_tray_icon()
 
     def _restorePosition(self):
         self._drag_position = QPoint()
@@ -23,6 +25,16 @@ class MainFrame(QMainWindow):
         p = toQPoint(str_pos)
         sz = QSize(200, 50)
         self.setGeometry(QRect(p, sz))
+
+    def _create_tray_icon(self):
+        self._tray = QSystemTrayIcon(LoadPNG(png_Boshi), self)
+
+        menu = QMenu()
+        exit_action = QAction("關閉", self)
+        exit_action.triggered.connect(QApplication.instance().quit)
+        menu.addAction(exit_action)
+        self._tray.setContextMenu(menu)
+        self._tray.show()
 
     def closeEvent(self, event):
         self._config.Save()
