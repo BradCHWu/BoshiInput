66fb548b5cd5c7ee7245c75ab8f433a1361a9668
Brad_Wu <Brad_Wu@wistron.com>
2026-04-10 13:01:48 +0800

    建立一個無邊框視窗 設定滑鼠在視窗上按下便可移動視窗

--- MainFrame.py
+++ MainFrame.py
@@ -1,7 +1,28 @@
 from PySide6.QtWidgets import QMainWindow
+from PySide6.QtCore import Qt, QPoint
+
 from setting import LoadPNG, png_Boshi
+
 
 class MainFrame(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
         self.setWindowIcon(LoadPNG(png_Boshi))
+
+        self._drag_position = None
+
+    def mousePressEvent(self, event):
+        if event.buttons() == Qt.MouseButton.LeftButton:
+            gp = event.globalPosition().toPoint()
+            topLeft = self.frameGeometry().topLeft()
+            self._drag_position = gp - topLeft
+
+        return super().mousePressEvent(event)
+
+    def mouseMoveEvent(self, event):
+        if event.buttons() == Qt.MouseButton.LeftButton:
+            gp = event.globalPosition().toPoint()
+            self.move(gp - self._drag_position)
+
+        return super().mouseMoveEvent(event)
