826d23a08ab7d757286cd8738d0c856bc41d0801
Brad_Wu <Brad_Wu@wistron.com>
2026-04-10 15:49:57 +0800

    加上 logging

--- BoshiInput.py
+++ BoshiInput.py
@@ -1,10 +1,13 @@
 import sys
+import logging
 
 from PySide6.QtWidgets import QApplication
 
 from MainFrame import MainFrame
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format="[%(levelname)s] %(message)s")
+
     app = QApplication(sys.argv)
     app.setQuitOnLastWindowClosed(False)
     window = MainFrame()
