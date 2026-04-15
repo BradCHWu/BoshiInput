dec629f37ac890440a050411d69b7e775569478d
Brad_Wu <Brad_Wu@wistron.com>
2026-04-15 13:26:00 +0800

    設定程式只能開啟一次

--- BoshiInput.py
+++ BoshiInput.py
@@ -1,11 +1,23 @@
 import sys
 
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QMessageBox
+from PySide6.QtCore import QSharedMemory
 
 from MainFrame import MainFrame
+from setting import Name
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+
+    shared_memory = QSharedMemory(Name())
+    if shared_memory.attach():
+        QMessageBox.warning(None, "提示", "程式正在執行中")
+        sys.exit(0)
+
+    if not shared_memory.create(1):
+        QMessageBox.warning(None, "提示", "無法建立共享記憶體")
+        sys.exit(0)
+
     app.setQuitOnLastWindowClosed(False)
     window = MainFrame()
     window.show()
